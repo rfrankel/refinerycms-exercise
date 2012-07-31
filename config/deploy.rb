7be5ef0917e6ab2104464c4214578fd716a0c858
Rebecca Frankel <rfrankel@alum.mit.edu>
2012-07-31 14:27:26 -0400

    changed the server in the deploy file since reverse DNS got enabled

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,9 +8,9 @@ set :deploy_to, "/home/deploy/apps/#{application}"
 set :use_sudo, false
 set :keep_releases, 5
 
-role :web, 50.116.53.19                         # Your HTTP server, Apache/etc
-role :app, 50.116.53.19                          # This may be the same as your `Web` server
-role :db,  50.116.53.19, :primary => true # This is where Rails migrations will run
+role :web, rebeccafrankel.com                         # Your HTTP server, Apache/etc
+role :app, rebeccafrankel.com                          # This may be the same as your `Web` server
+role :db,  rebeccafrankel.com, :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
