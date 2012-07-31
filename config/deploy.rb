19801dfaa24bb85950a6c57a6a29a40d465a7806
Rebecca Frankel <rfrankel@alum.mit.edu>
2012-07-31 14:28:43 -0400

    oops, forgot the quotes

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,9 +8,9 @@ set :deploy_to, "/home/deploy/apps/#{application}"
 set :use_sudo, false
 set :keep_releases, 5
 
-role :web, rebeccafrankel.com                         # Your HTTP server, Apache/etc
-role :app, rebeccafrankel.com                          # This may be the same as your `Web` server
-role :db,  rebeccafrankel.com, :primary => true # This is where Rails migrations will run
+role :web, "rebeccafrankel.com"                         # Your HTTP server, Apache/etc
+role :app, "rebeccafrankel.com"                          # This may be the same as your `Web` server
+role :db,  "rebeccafrankel.com", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
